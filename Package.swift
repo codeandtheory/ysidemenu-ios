e54fdb0082de50311543608454e820df452beb43
Caio Coan <caio.coan@ymedialabs.com>
2023-04-24 11:29:46 -0300

    Remove swiftlint plug in

--- Package.swift
+++ Package.swift
@@ -17,20 +17,19 @@ let package = Package(
         .package(
             url: "https://github.com/yml-org/YCoreUI.git",
             from: "1.7.0"
-        ),
-        .package(url: "https://github.com/adamayoung/swiftlint-plugin.git", from: "0.3.2")
+        )
     ],
     targets: [
         .target(
             name: "YSideMenu",
-            dependencies: ["YCoreUI"],
-            plugins: [
-                        .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
-                    ]
+            dependencies: ["YCoreUI"]
         ),
         .testTarget(
             name: "YSideMenuTests",
-            dependencies: ["YSideMenu"]
+            dependencies: ["YSideMenu"],
+            plugins: [
+                        .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
+                    ]
         )
     ]
 )
