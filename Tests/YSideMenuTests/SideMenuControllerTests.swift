bf027ebe1b891e537fe4fa6e9df452a5b2a03b37
CaioCoanYML <129888676+CaioCoanYML@users.noreply.github.com>
2023-04-26 08:32:40 -0300

    Create base SideMenuController (#2)
    * Create base SideMenuController
    
    * Add content to init
    
    * Fix swift lint
    
    * Remove swiftlint plug in
    
    * Fix build
    
    * Apply Mark suggestions
    
    * Update default dimmer opacity
    
    * Make rootviewcontroller let
    
    * Fix build
    
    * Apply Mark suggestions
    
    * Add unit tests
    
    ---------
    
    Co-authored-by: Mark Pospesel <mark.pospesel@ymedialabs.com>

--- Tests/YSideMenuTests/SideMenuControllerTests.swift
+++ Tests/YSideMenuTests/SideMenuControllerTests.swift
@@ -2,7 +2,7 @@
 //  SideMenuControllerTests.swift
 //  YSideMenu
 //
-//  Created by Dev Karan on 14/04/23.
+//  Created by Caio Coan on 14/04/23.
 //  Copyright © 2023 Y Media Labs. All rights reserved.
 //
 
@@ -10,10 +10,63 @@ import XCTest
 @testable import YSideMenu
 
 final class SideMenuControllerTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SideMenuController().text, "Hello, World!")
+    func test_init_createsChildController() throws {
+        let viewController = UIViewController()
+        let sut = makeSUT(rootViewController: viewController)
+        sut.loadViewIfNeeded()
+        
+        XCTAssertTrue(sut.children.contains(viewController))
+        XCTAssertEqual(viewController.parent, sut)
+        XCTAssertTrue(sut.contentView.subviews.contains(viewController.view))
+    }
+
+    func test_dimmerTap_dismissesSideMenu() {
+        let sut = makeSpy(rootViewController: UIViewController())
+        sut.loadViewIfNeeded()
+
+        XCTAssertFalse(sut.onDimmerTapped)
+        XCTAssertFalse(sut.isDismissed)
+
+        sut.simulateOnDimmerTap()
+
+        XCTAssertTrue(sut.onDimmerTapped)
+        XCTAssertTrue(sut.isDismissed)
+    }
+}
+
+private extension SideMenuControllerTests {
+    func makeSUT(
+        rootViewController: UIViewController,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> SideMenuController {
+        let sut = SideMenuController(rootViewController: rootViewController)
+        trackForMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+
+    func makeSpy(
+        rootViewController: UIViewController,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> SpySideMenuController {
+        let sut = SpySideMenuController(rootViewController: rootViewController)
+        trackForMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+}
+
+final class SpySideMenuController: SideMenuController {
+    var isDismissed = false
+    var onDimmerTapped = false
+
+    override func didDismiss() {
+        super.didDismiss()
+        isDismissed = true
+    }
+
+    override func simulateOnDimmerTap() {
+        super.simulateOnDimmerTap()
+        onDimmerTapped = true
     }
 }
